f40f65b263e49f99a9b5793774c8b18976b0d12c
Chris Goggans <cgogg1@gmail.com>
2022-01-20 18:51:21 -0500

    added musicPlayer css/js/html

--- script.js
+++ script.js
@@ -1,58 +1,50 @@
-import {key} from "./keys.js";
+import { key } from "./keys.js";
 
 const inputBox = document.getElementById("search-input");
 const searchButton = document.getElementById("submit-btn");
-//const audioObj = new Audio('https://api.deezer.com/album/258065882/tracks');
-//document.getElementById("results").appendChild(audioObj);
+
 searchButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    let musicSearch = inputBox.value;
-    console.log(musicSearch);
+  e.preventDefault();
+  let musicSearch = inputBox.value;
+  console.log(musicSearch);
 
-    fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${musicSearch}`, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-		"x-rapidapi-key": 
-    `${key}`
-	}
-})
-.then((response) => response.json())
- .then(responseData => {
-  	showResults(responseData.Search)})
-.then(response => {
-	console.log(response);
-})
-.catch((error) => {
-    console.log('Error:', error)
-})
-});
-
-// function showResults(results) {
-//     console.log(results)
-//     results.forEach(result => {
-//       let item = document.createElement("div");
-//       item.innerHTML = `<div class="card">
-//       <audio src="${result.preview}">
-//     </div>
-//     <br>` ;
-//       document.getElementById("result-div").appendChild(item);
-//     });
+  fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${musicSearch}`, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+      "x-rapidapi-key": `${key}`,
+    },
+  })
+    .then((response) => response.json())
     
-//    };
+    .then((response) => {
+      console.log(response);
+    })
+    .then((responseData) => {
+      showResults(responseData.Search);
+     });
+//     .catch((error) => {
+//       console.log("Error:", error);
+//     });
+// });
 
-   function showResults(results) {
-    console.log(results)
-    results.forEach(result => {
-      let item = document.createElement("div");
-      item.innerHTML = `<div class="card">
+function showResults(results) {
+  console.log(results);
+  results.map((result) => {
+    let item = document.createElement("div");
+    item.innerHTML = `<div class="card">
       <audio src="${result.preview}"></audio>
       <div class="card-body">
 
       </div>
     </div>
-    <br>` ;
-      document.getElementById("results").appendChild(item);
-    });
-    
-   };
+    <br>`;
+  });
+  document.getElementById("results").appendChild(item);
+}});
+
+// document.querySelector('#search-btn').addEventListener('keypress', function (e) {
+//   if (e.key === 'Enter') {
+//     // code for enter
+//   }
+// });
