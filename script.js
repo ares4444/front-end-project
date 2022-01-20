0a2c107bb9235f4748e4b20dd1e4284afb6e7708
Moises De Leon <mdeleon5826@gmail.com>
2022-01-20 12:16:25 -0500

    added songspage.js and updated index.html and songspage.html

--- script.js
+++ script.js
@@ -1,39 +1,15 @@
-import {key} from "./keys.js";
 
 const inputBox = document.getElementById("input");
-const searchButton = document.getElementById("button");
-const audioObj = new Audio('https://api.deezer.com/album/258065882/tracks');
-document.getElementById("results").appendChild(audioObj);
+const searchButton = document.getElementById("search-btn");
+
+
 searchButton.addEventListener("click", (e) => {
+    console.log("button clicked");
     e.preventDefault();
     let musicSearch = inputBox.value;
     console.log(musicSearch);
 
-    fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${musicSearch}`, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-		"x-rapidapi-key": 
-        `${key}`
-	}
-})
-.then((response) => response.json())
-//  .then(data => {
-//  	showResults(data.Search)})
+    localStorage.setItem("artist", musicSearch)
+    window.location.replace("http://127.0.0.1:5500/songspage.html");
 
-.catch((error) => {
-    console.log('Error:', error)
-})
-
-.then(data => {
-	console.log(data);
-    const dataResults = data.Search;
-dataResults.forEach(results => {
-    console.dir(results)
-      let item = document.createElement("div");
-      item.innerHTML = `<div class="card">
-      <audio controls src="${results.preview}" type="module"></audio></div>`;
-    //   document.getElementById("card-group").appendChild(item);
-    });
-   })
 });
