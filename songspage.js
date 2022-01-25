629956043ea146a3a9cdd0bb773d66a43bbd0287
Moises De Leon <mdeleon5826@gmail.com>
2022-01-25 15:06:47 -0500

    cleaned up the index.html as well as the songs.html, got the accordion to work

--- songspage.js
+++ songspage.js
@@ -11,7 +11,7 @@ console.log(musicSearch.artist);
 console.log(musicSearch.song);
 
 window.addEventListener('DOMContentLoaded', async (event) => {
-	let songResults = []
+	let songResults = [];
 	await fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${musicSearch.artist}`, {
   method: "GET",
   headers: {
@@ -30,6 +30,8 @@ window.addEventListener('DOMContentLoaded', async (event) => {
     const { data } = fetchResults;
     data.map((song) => {
 		let i = 0;
+		if(i < 26) {
+			i++;
 		let songCard = `<div id="music-container" class="music-container">
       <div class="card deezerApi"  style="width: 18rem;">
 		<img class="card-img-top" src="${song.album.cover_medium}" alt="Card image cap">
@@ -38,26 +40,23 @@ window.addEventListener('DOMContentLoaded', async (event) => {
       <br>
       <audio controls src="${song.preview}" id="audio" style="width: 100%"></audio>
     </div>
-	<div class="accordion" id="accordionExample">
-  		<div class="accordion-item">
-    		<h2 class="accordion-header" id="headingOne">
-				<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-				Accordion Item #1
-				</button>
-   		 	</h2>
-			<div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-				<div id="accordBody${i}" class="accordion-body"> 
-      			</div>
-    		</div>
-  		</div>
+	<div class="accordion accordion-flush" id="accordionFlushExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingOne">
+      <button id="accordionBtn" class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+        Accordion Item #1
+      </button>
+    </h2>
+    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      <div id="accordBody${i}" class="accordion-body"></div>
     </div>
+  </div>
   	</div>
 		</div>
 		</div>`;
-		
 			  results.innerHTML += songCard;
-			i++
-    	})
+			
+    	}})
 		//console.log(songArray)
 		return fetchResults;
 		
@@ -65,7 +64,7 @@ window.addEventListener('DOMContentLoaded', async (event) => {
 	console.log(songResults);
 	const accordBody = document.querySelectorAll('[id^="accordBody"]')
 	console.log(accordBody);
-	songResults.forEach(song => {
+	songResults.map(song => {
 		console.log(song);
 		let j = 0;
 		fetch(`https://private-amnesiac-3503f-lyricsovh.apiary-proxy.com/v1/${song.artist.name}/${song.title}`)
@@ -79,9 +78,10 @@ window.addEventListener('DOMContentLoaded', async (event) => {
 		// 		<p class="card-text">${data.lyrics}</p>
 		// 		</div>
 		// 	  </div>`
-			  accordBody[j].innerHTML = data.lyrics;
-	})
-	j++;
+			  accordBody[j].innerHTML = data.lyrics.replace(new RegExp("\n", "g"),"<br>");
+				j++;
+			})
+	
     })
 });
   
